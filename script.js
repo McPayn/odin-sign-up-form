b76181f0eef560ab6adbb184051187289e353cca
Tyler Brown <brown.tyler.w@gmail.com>
2023-09-14 20:36:15 -0700

    Added logo, minor style changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Ensures passwords match before submitting
+var first_name;
+var last_name;
+
 function confirmPassword() {
     const password = document.getElementById('first-password');
     const check_password = document.getElementById('second-password');
@@ -12,4 +15,9 @@ function confirmPassword() {
         check_password.style.border = '2px solid red';
         error_message.innerHTML = 'Passwords do not match!'
     }
+}
+
+function login() {
+    first_name = document.getElementById('first-name').value;
+    last_name = document.getElementById('last-name').value;
 }
