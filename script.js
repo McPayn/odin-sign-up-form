26de3212aaf8ac5bfe641d1566d3c526ac965168
Tyler <brown.tyler.w@gmail.com>
2023-09-18 09:34:52 -0700

    Style changes Added custom font and fixed format issues

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function confirmPassword() {
     const check_password = document.getElementById('second-password');
     const error_message = document.querySelector('.error');
     if (password.value === check_password.value) {
-        password.style.border = '2px solid green';
-        check_password.style.border = '2px solid green';
+        password.style.borderBottomColor = 'green';
+        check_password.style.borderBottomColor = 'green';
         error_message.innerHTML = '';
     } else {
-        password.style.border = '2px solid red';
-        check_password.style.border = '2px solid red';
+        password.style.borderBottomColor = 'red';
+        check_password.style.borderBottomColor = 'red';
         error_message.innerHTML = 'Passwords do not match!'
     }
 }
